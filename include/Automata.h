3f9b3b10be4f2f0da780115516659ea52560b7b3
zawdarkin <152159538+zawdarkin@users.noreply.github.com>
2025-05-16 13:39:37 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -3,43 +3,34 @@
 #define INCLUDE_AUTOMATA_H_
 
 #include <string>
-#include <utility>
 #include <vector>
+#include <ostream>
 
 enum class STATES { OFF, WAIT, ACCEPT, CHECK, COOK };
-enum class CHOISE_STATES { OK, INVALID_ITEM, NOT_ENOUGHT_MONEY, INACCESSIBLE };
-
-struct DrinkItem {
-    std::string drinkName;
-    double cost;
-};
 
 class Automata {
  private:
-    double totalRevenue = 0;
-    double currentDeposit = 0;
-    std::ostream& outputStream;
-    std::vector<DrinkItem> drinkMenu;
-    STATES currentMode = STATES::OFF;
+    double cash;
+    std::vector<std::string> menu;
+    std::vector<double> prices;
+    STATES state;
 
-    bool validateOrder(size_t itemIndex);
-    void prepareDrink();
-    void completeService();
-    std::pair<CHOISE_STATES, double> processChoiceResult(
-        std::pair<CHOISE_STATES, double> result);
+    bool check(int drink_index);
+    void cook(int drink_index);
+    void finish();
 
  public:
+    Automata();
     explicit Automata(std::ostream& os);
-    Automata(std::ostream& os, std::vector<DrinkItem> customMenu);
 
     void on();
     void off();
     void coin(double amount);
-    std::vector<DrinkItem> getMenu();
-    double getCashe();
+    std::vector<std::string> getMenu();
+    std::vector<double> getPrices();
     STATES getState();
-    std::pair<CHOISE_STATES, double> choice(size_t drinkIndex);
-    double cancel();
+    void choice(int drink_index);
+    void cancel();
 };
 
 #endif  // INCLUDE_AUTOMATA_H_
