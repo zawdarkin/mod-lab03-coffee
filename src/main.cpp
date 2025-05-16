60086dc509d0c913df6dcd503700f731e9878227
zawdarkin <152159538+zawdarkin@users.noreply.github.com>
2025-05-16 14:05:16 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,15 @@
 
 int main() {
     Automata* coffeeMachine = new Automata(std::cout);
-    coffeeMachine->powerOn();
-    coffeeMachine->showMenu();
-    coffeeMachine->getCurrentBalance();
-    coffeeMachine->insertMoney(20.0);
-    coffeeMachine->getCurrentBalance();
-    coffeeMachine->getCurrentStatus();
-    coffeeMachine->selectBeverage(2);
-    coffeeMachine->insertMoney(100.0);
-    coffeeMachine->selectBeverage(2);
+    coffeeMachine->on();
+    coffeeMachine->getMenu();
+    coffeeMachine->getCashe();
+    coffeeMachine->coin(20.0);
+    coffeeMachine->getCashe();
+    coffeeMachine->getState();
+    coffeeMachine->choice(2);
+    coffeeMachine->coin(100.0);
+    coffeeMachine->choice(2);
     delete coffeeMachine;
     return 0;
 }
