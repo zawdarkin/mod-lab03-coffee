36bc2ad5cff3906269df75b1ac345f493a93044b
zawdarkin <152159538+zawdarkin@users.noreply.github.com>
2025-05-16 13:53:38 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,36 +1,58 @@
-// Copyright 2022 UNN-IASR
-#ifndef INCLUDE_AUTOMATA_H_
-#define INCLUDE_AUTOMATA_H_
+// Copyright 2023 BeverageTech
+#ifndef BEVERAGE_AUTOMATA_H_
+#define BEVERAGE_AUTOMATA_H_
 
 #include <string>
 #include <vector>
+#include <utility>
 #include <ostream>
 
-enum class STATES { OFF, WAIT, ACCEPT, CHECK, COOK };
-
-class Automata {
- private:
-    double cash;
-    std::vector<std::string> menu;
-    std::vector<double> prices;
-    STATES state;
-
-    bool check(int drink_index);
-    void cook(int drink_index);
-    void finish();
-
- public:
-    Automata();
-    explicit Automata(std::ostream& os);
-
-    void on();
-    void off();
-    void coin(double amount);
-    std::vector<std::string> getMenu();
-    std::vector<double> getPrices();
-    STATES getState();
-    void choice(int drink_index);
-    void cancel();
+enum class MachineState { 
+    POWER_OFF, 
+    IDLE, 
+    PAYMENT_ACCEPTING, 
+    ORDER_VERIFICATION, 
+    BEVERAGE_PREPARATION 
 };
 
-#endif 
+enum class OrderStatus { 
+    SUCCESS, 
+    INVALID_SELECTION, 
+    INSUFFICIENT_FUNDS, 
+    UNAVAILABLE 
+};
+
+struct BeverageOption {
+    std::string name;
+    double price;
+};
+
+class BeverageMachine {
+ private:
+    double totalSales;
+    double currentBalance;
+    std::ostream& output;
+    std::vector<BeverageOption> beverageOptions;
+    MachineState currentStatus;
+
+    bool verifyOrder(size_t itemIndex);
+    void startPreparation();
+    void finalizeTransaction();
+    std::pair<OrderStatus, double> processOrderResult(
+        std::pair<OrderStatus, double> result);
+
+ public:
+    explicit BeverageMachine(std::ostream& os);
+    BeverageMachine(std::ostream& os, std::vector<BeverageOption> customOptions);
+
+    void powerOn();
+    void powerOff();
+    void insertMoney(double amount);
+    std::vector<BeverageOption> showMenu();
+    double getCurrentBalance();
+    MachineState getCurrentStatus();
+    std::pair<OrderStatus, double> selectBeverage(size_t beverageIndex);
+    double cancelTransaction();
+};
+
+#endif  // BEVERAGE_AUTOMATA_H_
