7e55fae9ac2bdb1fa1defdc76ce05b69d2f57ba6
zawdarkin <152159538+zawdarkin@users.noreply.github.com>
2025-05-16 13:40:37 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,144 +1,72 @@
 // Copyright 2022 GHA Test Team
 #include <gtest/gtest.h>
-#include <utility>
-#include <iostream>
-#include <sstream>
-#include <string>
-
 #include "Automata.h"
 
-TEST(task1, test1) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  ASSERT_EQ(STATES::OFF, automata->getState());
-  EXPECT_EQ(strs.str(), "OFF\n");
+TEST(AutomataTest, InitialState) {
+    Automata automata;
+    ASSERT_EQ(automata.getState(), STATES::OFF);
 }
 
-TEST(task1, test2) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  ASSERT_EQ(STATES::WAIT, automata->getState());
-  EXPECT_EQ(strs.str(), "WAIT\n");
+TEST(AutomataTest, TurnOn) {
+    Automata automata;
+    automata.on();
+    ASSERT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST(task1, test3) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  automata->off();
-  ASSERT_EQ(STATES::OFF, automata->getState());
-  EXPECT_EQ(strs.str(), "OFF\n");
+TEST(AutomataTest, TurnOff) {
+    Automata automata;
+    automata.on();
+    automata.off();
+    ASSERT_EQ(automata.getState(), STATES::OFF);
 }
 
-TEST(task1, test4) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  automata->on();
-  ASSERT_EQ(STATES::WAIT, automata->getState());
-  EXPECT_EQ(strs.str(), "WAIT\n");
+TEST(AutomataTest, InsertCoin) {
+    Automata automata;
+    automata.on();
+    automata.coin(20);
+    ASSERT_EQ(automata.getState(), STATES::ACCEPT);
 }
 
-TEST(task1, test5) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->off();
-  ASSERT_EQ(STATES::OFF, automata->getState());
-  EXPECT_EQ(strs.str(), "OFF\n");
+TEST(AutomataTest, GetMenu) {
+    Automata automata;
+    auto menu = automata.getMenu();
+    ASSERT_FALSE(menu.empty());
 }
 
-TEST(task2, test1) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  automata->coin(20);
-  STATES result = automata->getState();
-  ASSERT_EQ(STATES::ACCEPT, result);
-  std::pair<CHOISE_STATES, double> cookResult = automata->choice(2);
-  ASSERT_EQ(CHOISE_STATES::NOT_ENOUGHT_MONEY, cookResult.first);
-  EXPECT_EQ(20, cookResult.second);
+TEST(AutomataTest, CheckPrices) {
+    Automata automata;
+    auto prices = automata.getPrices();
+    ASSERT_EQ(prices.size(), 5);
 }
 
-TEST(task2, test2) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  std::pair<CHOISE_STATES, double> cookResult = automata->choice(0);
-  ASSERT_EQ(CHOISE_STATES::INACCESSIBLE, cookResult.first);
-  EXPECT_EQ(0, cookResult.second);
+TEST(AutomataTest, SuccessfulPurchase) {
+    Automata automata;
+    automata.on();
+    automata.coin(50);
+    automata.choice(1);  // Americano
+    ASSERT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST(task2, test3) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  automata->coin(100);
-  std::pair<CHOISE_STATES, double> cookResult = automata->choice(2);
-  ASSERT_EQ(CHOISE_STATES::OK, cookResult.first);
-  EXPECT_EQ(65, cookResult.second);
+TEST(AutomataTest, InsufficientFunds) {
+    Automata automata;
+    automata.on();
+    automata.coin(10);
+    automata.choice(2);  // Cappuccino
+    ASSERT_EQ(automata.getState(), STATES::ACCEPT);
 }
 
-TEST(task2, test4) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  automata->coin(20);
-  std::pair<CHOISE_STATES, double> cookResult = automata->choice(-2);
-  ASSERT_EQ(CHOISE_STATES::INVALID_ITEM, cookResult.first);
-  EXPECT_EQ(20, cookResult.second);
+TEST(AutomataTest, CancelOperation) {
+    Automata automata;
+    automata.on();
+    automata.coin(30);
+    automata.cancel();
+    ASSERT_EQ(automata.getState(), STATES::WAIT);
 }
 
-TEST(task2, test5) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  automata->coin(-10);
-  ASSERT_EQ(0, automata->getCashe());
-  EXPECT_EQ(STATES::WAIT, automata->getState());
-}
-
-TEST(task2, test6) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  automata->coin(10);
-  automata->coin(-10);
-  ASSERT_EQ(10, automata->getCashe());
-  EXPECT_EQ(STATES::ACCEPT, automata->getState());
-}
-
-TEST(task2, test7) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->coin(10);
-  ASSERT_EQ(0, automata->getCashe());
-  EXPECT_EQ(STATES::OFF, automata->getState());
-}
-
-TEST(task2, test8) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  automata->coin(100);
-  automata->choice(2);
-  automata->coin(1000);
-  ASSERT_EQ(1000, automata->getCashe());
-  EXPECT_EQ(STATES::ACCEPT, automata->getState());
-}
-
-TEST(task3, test1) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  size_t result = (automata->getMenu()).size();
-  ASSERT_EQ(0, result);
-  EXPECT_EQ(strs.str(), "");
-}
-
-TEST(task3, test2) {
-  std::stringstream strs;
-  Automata* automata = new Automata(strs);
-  automata->on();
-  size_t result = (automata->getMenu()).size();
-  EXPECT_EQ(5, result);
+TEST(AutomataTest, InvalidChoice) {
+    Automata automata;
+    automata.on();
+    automata.coin(50);
+    automata.choice(10);  // Несуществующий напиток
+    ASSERT_EQ(automata.getState(), STATES::ACCEPT);
 }
